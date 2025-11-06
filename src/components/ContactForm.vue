2659be8c4a7c7dbb57f8ba01c0b80a422ac66d2c
Luis Sousa <las.sousa@outlook.com>
2025-11-06 16:59:54 -0300

    add v-model + objeto reativo

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,4 +1,20 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+
+const dataForm = reactive({
+  firstName: '',
+  lastName: '',
+  email: '',
+  subject: '',
+  message: '',
+  acceptedTerms: false,
+})
+
+const handleSubmit = () => {
+  const dados = { ...dataForm }
+  console.log(dados)
+}
+</script>
 
 <template>
   <form class="container-form">
@@ -6,37 +22,43 @@
     <div class="container-name">
       <div class="input-label">
         <label for="firstName">Nome</label>
-        <input type="text" id="firstName" />
+        <input type="text" id="firstName" v-model="dataForm.firstName" />
       </div>
       <div class="input-label">
         <label for="lastName">Sobrenome</label>
-        <input type="text" id="lastName" />
+        <input type="text" id="lastName" v-model="dataForm.lastName" />
       </div>
     </div>
     <div class="container-email">
       <label for="email">Email</label>
-      <input type="email" name="email" id="email" />
+      <input type="email" name="email" id="email" v-model="dataForm.email" />
     </div>
     <label for="assunto">Motivo do contato</label>
     <div class="container-assunto">
       <label for="geral" class="radio-input">
-        <input type="radio" name="assunto" id="geral" value="geral" />
+        <input type="radio" name="assunto" id="geral" value="geral" v-model="dataForm.subject" />
         <span>Assuntos gerais</span>
       </label>
       <label for="suporte" class="radio-input">
-        <input type="radio" name="assunto" id="suporte" value="suporte" />
+        <input
+          type="radio"
+          name="assunto"
+          id="suporte"
+          value="suporte"
+          v-model="dataForm.subject"
+        />
         <span>Pedido de suporte</span>
       </label>
     </div>
     <div class="container-msg">
       <label for="mensagem">Mensagem</label>
-      <textarea name="mensagem" id="mensagem" />
+      <textarea name="mensagem" id="mensagem" v-model="dataForm.message" />
     </div>
     <div class="container-termos">
-      <input type="checkbox" name="termos" id="termos" />
+      <input type="checkbox" name="termos" id="termos" v-model="dataForm.acceptedTerms" />
       <label for="termos">Eu aceito os termos.</label>
     </div>
-    <button type="button">Enviar</button>
+    <button type="button" @click="handleSubmit">Enviar</button>
   </form>
 </template>
 
@@ -44,6 +66,7 @@
 .container-form {
   background-color: var(--white);
   max-width: 640px;
+  /* min-width: 375px; */
   margin: 64px auto;
   padding: 24px;
   border-radius: 12px;
